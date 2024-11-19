2e3307d963596f180d6ca664afd6183b22ff12ff
vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2024-11-19 14:52:02 +0100

    add chats list and window

---
+++ src/lib/components/ChatList.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let chats = [
+        { img_src: '', title: 'Name1', message: 'lorem bla bla' },
+        { img_src: '', title: 'Name2', message: 'lorem bla bla bla bla bla bla bla' }
+    ]
+</script>
+
+{#each chats as { img_src, title, message }}
+    <section class="chat">
+        <img src={img_src} alt={title} class="profile-picture">
+        <h3 class="chat-name">{title}</h3>
+        <p class="chat-message">{message}</p>
+    </section>  
+{/each}
--- src/routes/(app)/user/+page.svelte
+++ src/routes/(app)/user/+page.svelte
@@ -1,22 +1,22 @@
+<script>
+    import ChatList from '$lib/components/ChatList.svelte';
+    let chats = [
+        { img_src: '', title: 'Name1', message: 'lorem bla bla' },
+        { img_src: '', title: 'Name2', message: 'lorem bla bla bla bla bla bla bla' }
+    ]
+</script>
+
 <svelte:head>
     <title>Chats</title>
 </svelte:head>
 
 <div id="wrapper">
-    <section id="chats">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, sunt pariatur repudiandae voluptatem sequi praesentium aut necessitatibus alias minima voluptatum neque ipsum ab aperiam! Natus quidem fugit incidunt laudantium numquam?</p>
-        <p>Velit, nostrum ipsum. Quia deserunt repudiandae accusamus, a exercitationem at, culpa iste corrupti, eveniet consequatur omnis adipisci. Labore veniam impedit, aliquid laboriosam natus ut. Placeat voluptates quos distinctio quo eligendi.</p>
-        <p>In iste architecto distinctio cum natus expedita non ratione voluptatem magni? Magni officia ullam porro neque itaque, excepturi, obcaecati eius, vel rem quisquam commodi? Molestiae sed quaerat corrupti velit quo.</p>
-        <p>Delectus fugit vel distinctio sequi quae voluptate minus ipsa totam, ipsam eaque, eius incidunt harum, nemo consequuntur illo praesentium corrupti repellat ad laborum? Minima dolores expedita beatae ipsam rerum eligendi!</p>
-        <p>Aliquam, optio nobis quia ullam dolorem eveniet omnis ad accusantium exercitationem quisquam quos nostrum. Quos pariatur deleniti minus quisquam ex eaque, quam asperiores ducimus saepe fugit quia. Consequuntur, impedit quis.</p>
-        <p>Ducimus similique cupiditate eos ullam ipsa quasi. Dolor illum illo animi deserunt voluptatibus quas saepe alias porro debitis amet itaque, nihil reprehenderit sit blanditiis rerum accusamus omnis temporibus ipsum ipsam?</p>
-        <p>Quia, a voluptatum! Harum officia, fugit ipsa a provident expedita soluta nihil fuga optio odio sunt mollitia facere inventore magni explicabo, facilis voluptatum quia molestiae voluptatem! Id sequi eligendi nihil?</p>
-        <p>Repellendus nemo eligendi ea nostrum vitae non labore, assumenda quaerat deleniti quidem ducimus, quasi molestiae quisquam autem temporibus in, nobis sunt similique excepturi culpa! Explicabo nisi accusamus incidunt illo distinctio.</p>
-        <p>Vitae reprehenderit quia culpa velit? Distinctio possimus cupiditate, dicta, facilis obcaecati iste, enim modi eius incidunt corporis quo dolor animi cumque ullam a expedita nulla explicabo suscipit molestiae dolore mollitia.</p>
-        <p>Harum cupiditate aspernatur impedit soluta asperiores tempora, suscipit modi nam? Quisquam tempore quaerat suscipit amet nesciunt sed reiciendis a nemo voluptate, molestias beatae dignissimos repellat adipisci nulla consectetur, maxime eveniet.</p>
+    <h1>Chats</h1>
+    <section id="chats-list">
+        <ChatList {chats} />
     </section>
-    
-    <section id="chat">
+
+    <section id="chat-display">
         <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, fugiat. Inventore veritatis doloremque voluptate iure nesciunt assumenda qui alias temporibus beatae dolores dolore aspernatur laudantium, cupiditate sint, unde esse earum.</h1>
         <h1>Alias illo dolore ducimus in, nesciunt cumque. Nisi mollitia quisquam ipsa quis cupiditate repudiandae. Error eaque nesciunt reiciendis? Similique deserunt debitis ex fuga magnam porro enim voluptatem ad voluptatum temporibus.</h1>
         <h1>Labore, libero. Neque minima doloremque eveniet dolorem autem, perferendis ex asperiores labore aliquam amet rem ab libero repellendus dolores sit nulla id facilis? Tempora nemo, praesentium laudantium dolores debitis odio!</h1>
@@ -28,10 +28,54 @@
 <style lang="scss">
     #wrapper {
         display: grid;
-        grid-template-columns: 3fr 7fr;
+        grid-template-columns: minmax(14rem, 7fr) 13fr;
+        grid-template-rows: auto 1fr;
+        grid-gap: 0 3rem;
+        max-height: calc(94vh - 2 * var(--main-padding));
+        overflow: hidden;
+        
+        #chats-list {
+            grid-column: 1;
 
-        #chats {
-            padding: 1rem 0;
+            overflow-y: auto;
+            scrollbar-width: none;
+
+            .chat {
+                display: grid;
+                grid-template-columns: auto 1fr;
+                grid-template-rows: auto minmax(2rem, 5rem);
+                grid-gap: 0.4rem 1rem;
+                align-items: center;
+                padding: 0.4rem;
+                border: 1px solid var(--secondary-bg-color); 
+                
+                .profile-picture {
+                    grid-column: 1;
+                    grid-row: span 2;
+                }
+
+                .chat-name {
+                    grid-column: 2;
+                    grid-row: 1;
+                }
+                
+                .chat-message {
+                    grid-column: 2;
+                    grid-row: 2;
+                    max-height: 5rem;
+                    overflow: hidden;
+                }
+
+                &:hover {
+                    cursor: pointer;
+                    background-color: var(--primary-hover-bg-color);
+                    color: var(--primary-hover-text-color);
+                }
+            }
+        }
+        
+        #chat-display {
+            grid-column: 2  ;
         }
     }
 </style>
